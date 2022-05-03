4765c2fbd07296ad086c828957fb51be51836e31
pppp0722 <poj0722@naver.com>
2022-05-03 15:25:31 +0900

    [init] spring boot + react 초기 설정

---
+++ src/main/java/com/pppp0722/shoesstore/ShoesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.pppp0722.shoesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesStoreApplication.class, args);
+    }
+
+}
